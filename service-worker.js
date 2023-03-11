2576ac6d8236f58bd1396ad543b95d73f0eab86f
rubensostenes <rubenmelo332@gmail.com>
2023-03-11 20:37:17 -0300

    Adição de todo o javt

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,18 @@
 // 
 
-
 const CACHE = "pwabuilder-offline-v2";
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js%27');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 self.addEventListener("message", (event) => {
-    if (event.data && event.data.type === "SKIP_WAITING") {
-        self.skipWaiting();
-    }
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
 
 workbox.routing.registerRoute(
-    new RegExp('/*'),
-    new workbox.strategies.StaleWhileRevalidate({
-        cacheName: CACHE
-    })
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
 );
