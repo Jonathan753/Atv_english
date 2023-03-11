8116026ebe693bdfdb38a251f319f1e8f936d2c2
rubensostenes <rubenmelo332@gmail.com>
2023-03-11 19:49:53 -0300

    Adição de todo o javascript

--- intarecao.js
+++ intarecao.js
@@ -7,6 +7,10 @@ function openNav() {
     document.getElementById("mySidebar").style.display = "none";
   }
 
+
+
+
+
 const handleSubmit = (event) => {
   event.preventDefault();
 
@@ -25,3 +29,36 @@ const handleSubmit = (event) => {
 document
   .querySelector("form")
   .addEventListener("submit", handleSubmit);
+
+
+
+
+
+
+  
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js').then(reg => {
+      reg.addEventListener('updatefound', () => {
+        // A wild service worker has appeared in reg.installing!
+        newWorker = reg.installing;
+        newWorker.addEventListener('statechange', () => {
+          // Has network.state changed?
+          switch (newWorker.state) {
+            case 'installed':
+              if (navigator.serviceWorker.controller) {
+                // new update available
+                showUpdateBar();
+              }
+              // No update available
+              break;
+          }
+        });
+      });
+    });
+    let refreshing;
+    navigator.serviceWorker.addEventListener('controllerchange', function () {
+      if (refreshing) return;
+      window.location.reload();
+      refreshing = true;
+    });
+  }
---
+++ service-worker.js
@@ -0,0 +1,59 @@
+var cacheName = 'pwaTeste+-v1.2';
+
+self.addEventListener('install', event => {
+
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll([
+
+        './index.html',
+
+        './interacao.js',
+
+        './foto1.png',
+        './download.png',
+        './baixados.png',
+        './22962.png',
+        './icones/128.png',
+        './icones/144.png',
+        './icones/152.png',
+        './icones/167.png',
+        './icones/180.png',
+        './icones/192.png',
+        './icones/256.png',
+        './icones/512.png',
+        
+      ]))
+  );
+});
+
+self.addEventListener('message', function (event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('fetch', function (event) {
+  //Atualizacao internet
+  event.respondWith(async function () {
+     try {
+       return await fetch(event.request);
+     } catch (err) {
+       return caches.match(event.request);
+     }
+   }());
+
+  //Atualizacao cache
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+
+});
