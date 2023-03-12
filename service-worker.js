ae4cd6869f4c458e0bddfe8c03872daae25b75a9
rubensostenes <rubenmelo332@gmail.com>
2023-03-11 22:13:36 -0300

    Adição de todot

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,62 @@
+var cacheName = 'pwaTeste+-v1.2';
+
+self.addEventListener('install', event => {
+
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll([
+
+        '/index.html',
+
+        '/interacao.js',
+
+        '/foto1.png',
+        '/download.png',
+        '/baixados.png',
+        '/22962.png',
+        '/icones/48.png',
+        '/icones/72.png',
+        '/icones/96.png',
+        '/icones/144.png',
+        '/icones/192.png',
+        '/icones/512.png',
+        
+      ]))
+  );
+});
+
+self.addEventListener('message', function (event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('fetch', function (event) {
+  //Atualizacao internet
+  event.respondWith(async function () {
+     try {
+       return await fetch(event.request);
+     } catch (err) {
+       return caches.match(event.request);
+     }
+   }());
+
+  //Atualizacao cache
+  /*event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );*/
+
+});
+
+
 // This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
 const CACHE = "pwabuilder-offline-page";
