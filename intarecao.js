15b1f0db7fbcb14f15bf4a3ac725436c71d3331c
rubensostenes <rubenmelo332@gmail.com>
2023-03-12 16:47:38 -0300

    Adição de t

--- intarecao.js
+++ intarecao.js
@@ -1,3 +1,4 @@
+//barra do lado
 function openNav() {
     document.getElementById("mySidebar").style.width = "60%";
     document.getElementById("mySidebar").style.display = "block";
@@ -7,7 +8,7 @@ function openNav() {
     document.getElementById("mySidebar").style.display = "none";
   }
 
-  
+//form
 const handleSubmit = (event) => {
   event.preventDefault();
 
@@ -32,3 +33,31 @@ document
   if (typeof navigator.serviceWorker !== 'undefined') {
     navigator.serviceWorker.register('sw.js')
   }
+
+//gigas
+
+document.getElementById('result').innerHTML = navigator.deviceMemory || 'unknown'
+
+
+//geolocalização
+
+var target = document.getElementById('target');
+var watchId;
+
+function appendLocation(location, verb) {
+  verb = verb || 'updated';
+  var newLocation = document.createElement('p');
+  newLocation.innerHTML = 'Location ' + verb + ': ' + location.coords.latitude + ', ' + location.coords.longitude + '';
+  target.appendChild(newLocation);
+}
+
+if ('geolocation' in navigator) {
+  document.getElementById('askButton').addEventListener('click', function () {
+    navigator.geolocation.getCurrentPosition(function (location) {
+      appendLocation(location, 'fetched');
+    });
+    watchId = navigator.geolocation.watchPosition(appendLocation);
+  });
+} else {
+  target.innerText = 'Geolocation API not supported.';
+}
